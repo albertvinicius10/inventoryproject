a550d1a5f7282adbd1777fe1a471ed907de03517
albertvinicius10 <albertvinicius10@gmail.com>
2022-09-13 14:26:12 -0400

    Updated

---
+++ dashboard/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Product
+from django.contrib.auth.models import Group
+# Register your models here.
+admin.site.site_header = 'KenInventory Dashboard'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','category','quantity',)
+    list_filter = ('category',)
+
+admin.site.register(Product, ProductAdmin)
+#admin.site.unregister(Group)
