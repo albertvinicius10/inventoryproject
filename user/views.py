7b8a2e4e6304fada22d6df90fe541a21993757e0
albertvinicius <albertvinicius10@gmail.com>
2022-09-21 13:31:07 -0400

    update

--- user/views.py
+++ user/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
-
+from django.contrib import messages
 # Create your views here.
 def register(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account has been created for{username}. Continue to Log in')
             return redirect('user-login')
     else:
         form = CreateUserForm()
